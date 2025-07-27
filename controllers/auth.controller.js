917383a6e94cfe9a08217124ba15c97518217e86
mansi9028 <153229504+mansi9028@users.noreply.github.com>
2025-07-27 07:07:48 +0000

    adjusting controllers

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,15 +1,43 @@
-import { mongoose } from "mongoose"
+import { mongoose } from "mongoose";
+import bcrypt from "bcryptjs";
+import User from "../models/user.model.js";
+import {jwt} from "jsonwebtoken";
+import { JWT_EXPIRES_IN } from "../config/env.js";
+
 
 export const signUp = async (req, res, next) => {
     const session = await mongo.startSession();
     session.startTransaction();
 
     try {
-        
+        const { name, email, password } = req.body;
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            const error = new Error("User already exists");
+            error.statusCode = 409;
+            throw error;
+        }
 
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
 
+        const user = await User.create([{ name, email, password: hashedPassword }], {session});
+
+        const token = jwt.sign(
+            { userId: user._id, email: user.email },
+            process.env.JWT_SECRET,
+            { expiresIn: "1h" }
+        );
 
         await session.commitTransaction();
+        session.endSession();
+
+        res.status(201).json({
+            message: "User created successfully",
+            userId: user._id,
+            token: token
+        });
+
     } catch (error) {
         await session.abortTransaction();
         session.endSession();
@@ -19,4 +47,4 @@ export const signUp = async (req, res, next) => {
 
 export const signIn = async (req, res, next) => {}
 
-export const signOut = async (req, res, next) => {
+export const signOut = async (req, res, next) => {}
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,10 +3,14 @@ import { signUp } from '../controllers/auth.controller';
 import { signIn } from '../controllers/auth.controller';
 import { signOut } from '../controllers/auth.controller';
 
+
 const authRouter = Router();
+
 
 authRouter.post('/sign-up', signUp);
 authRouter.post('/sign-in', signIn);
 authRouter.post('/sign-out', signOut);
+
+
 
 export default authRouter;
