f74c65432f78271d35d4759460dae3842018ca4a
mansi9028 <153229504+mansi9028@users.noreply.github.com>
2025-07-27 07:11:35 +0000

    adding new routes

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,2 +1,25 @@
-import User from "../models/user.model";    
+import User from "../models/user.model.js";    
 
+export const getUsers = async (req, res, next) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const getUser = async (req, res, next) => {
+    const userId = req.params.id;
+    try {
+        const user = await User.findById(userId);
+        if (!user) {
+            const error = new Error("User not found");
+            error.statusCode = 404;
+            throw error;
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        next(error);
+    }
+}
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,17 @@
 import { Router } from 'express';
+import { getAllUsers } from '../controllers/user.controller';
+import { getUser } from '../controllers/user.controller';
+import { createUser } from '../controllers/user.controller';
+import { updateUser } from '../controllers/user.controller';
+import { deleteUser } from '../controllers/user.controller';
 
 const userRouter = Router();
 
-userRouter.get('/', (req, res) => res.send( {title: 'GET all users'}));
+userRouter.get('/', getAllUsers);
+userRouter.get('/:id', getUser);
+userRouter.post('/', createUser);
+userRouter.put('/:id', updateUser);
+userRouter.delete('/:id', deleteUser);
 
 userRouter.get('/:id', (req, res) => res.send( {title: 'GET user details'}));
 
